85e80a6e38de4bccf2d43e337d56504eb0a3d7a0
Malyellen Vang <50208740+MalyElle@users.noreply.github.com>
2019-05-31 15:04:55 -0500

    Add files via upload

---
+++ Fruit.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceHW
+{
+    public class Fruit
+    {
+        public string _type { set; get; }
+        public string _taste { set; get; }
+
+
+        public Fruit(string type, string taste)
+        {
+            _type = type;
+            _taste = taste;
+
+            Console.WriteLine("Fruit have been born for human to eat!");
+        }
+
+        public void texture ( string textureTest)
+        {
+            Console.WriteLine("The forbidden {0} is no longer forbidden and its taste is {1}! The {0} is so {2}, that I never can imagine a taste like this! ", _type, _taste, textureTest);
+        }
+            
+    }
+
+    public class Berries : Fruit
+    {
+        public string _color { set; get; }
+        public int _quantity { set; get; }
+
+        public Berries(string type, string taste, string color, int quantity) : base (type,taste)
+        {
+            _type = type;
+            _taste= taste;
+            _color = color;
+            _quantity = quantity;
+        }
+
+        public void amountBerries (int timesEaten)
+        {
+            for(int i = 0; i<timesEaten; i++)
+            {
+                Console.WriteLine("Humans you have took advantage of what I gave you and consume over {0} of my {1} {2}!",_quantity,_color,_type);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InheritanceHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Berries SB = new Berries("StrawBerry", "Sweet", "Red", 1000);
+            Berries BB = new Berries("BlackBerry", "Sweet and Sour", "Black", 500);
+
+            SB.texture("super moist and mushy");
+            BB.texture("very mushy");
+
+            SB.amountBerries(2);
+            BB.amountBerries(3);
+        }
+    }
+}
